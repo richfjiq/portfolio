52146f0ac5370c04c4483d9a4b96848542a2dfd0
richfjiq <rfjiq1986@gmail.com>
2023-03-14 16:54:12 -0600

    updates skills page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			setPreloading(false);
-		}, 3000);
+		}, 1500);
 
 		return () => clearTimeout(timer);
 	}, []);
--- pages/about.tsx
+++ pages/about.tsx
@@ -10,20 +10,20 @@ const About: FC = () => {
 		<Navbar title="About">
 			<div className="max-w-[1240px] m-auto gap-8">
 				<div className={`flex justify-center ${styles.headerContainer}`}>
-					<h1 className={`mb-5 mt-5 min-[440px]:mt-0 text-center`}>About me</h1>
+					<h1 className={`mb-5 mt-5 min-[700px]:mt-0 text-center`}>About me</h1>
 				</div>
 				<div className={`grid grid-cols-12 ${styles.container}`}>
 					<div
 						className={`col-start-2 col-span-10 min-[700px]:col-start-1 min-[700px]:pl-10 min-[700px]:col-span-6 flex flex-col justify-center`}
 					>
-						<p className="py-5 text-justify text-[#f7f1e3]-600 text-base min-[700px]:text-left min-[700px]:text-xl min-[1100px]:text-2xl">
+						<p className="py-5 text-justify text-[#f7f1e3]-600 text-base min-[700px]:text-left min-[700px]:text-xl">
 							I&apos;m a full-stack junior developer. I&apos;m enthusiastic about acquiring more
 							knowledge and experience that will allow me to be a better developer.
 						</p>
-						<p className="text-[#f7f1e3]-600 text-justify min-[700px]:text-left text-base min-[700px]:text-xl min-[1100px]:text-2xl">
+						<p className="text-[#f7f1e3]-600 text-justify min-[700px]:text-left text-base min-[700px]:text-xl">
 							I&apos;m a well-organized person, problem solver, with high attention to detail.
 						</p>
-						<p className="py-5 text-justify min-[700px]:text-left text-[#f7f1e3]-600 text-base min-[700px]:text-xl min-[1100px]:text-2xl">
+						<p className="py-5 text-justify min-[700px]:text-left text-[#f7f1e3]-600 text-base min-[700px]:text-xl">
 							Interested in the entire frontend spectrum, so the backend and working on ambitious
 							projects with positive people.
 						</p>
@@ -33,7 +33,7 @@ const About: FC = () => {
 					</div>
 				</div>
 
-				<div className={styles.btnContainer}>
+				<div className={`mt-5 mb-14 min-[700px]:my-5 ${styles.btnContainer}`}>
 					<Link
 						href="/projects"
 						className={`text-base min-[600px]:text-xl min-[1100px]:text-2xl ${styles.button}`}
