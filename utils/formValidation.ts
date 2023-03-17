c8476c605deb404efd84f09e6d10e2d1bf23ba94
richfjiq <rfjiq1986@gmail.com>
2023-03-17 17:31:15 -0600

    fix: custom error message for form validation - yup

--- utils/formValidation.ts
+++ utils/formValidation.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 export const formValidation = yup.object().shape({
-	name: yup.string().min(4, 'at least 4 characters').required(),
-	email: yup.string().email().required(),
-	subject: yup.string().min(5).required(),
-	message: yup.string().min(10).required(),
+	name: yup.string().min(4, 'At least 4 characters').required(),
+	email: yup.string().email('Email must be a valid email').required('Email is a required field'),
+	subject: yup.string().min(5, 'At least 5 characters').required(),
+	message: yup.string().min(10, 'At least 10 characters').required(),
 });
